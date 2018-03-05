d45d2c86de9ebf062e32cbf3ec5b8fe79c584092
Santiago Barroso <sbarroso.dev@gmail.com>
2018-03-05 20:40:22 +0100

    completed app

---
+++ src/app/lessons-list/lessons-list.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Lesson} from "../shared/model/lesson";
+import * as _ from 'lodash';
+import {Observer} from 'rxjs';
+
+@Component({
+    selector: 'lessons-list',
+    templateUrl: './lessons-list.component.html',
+    styleUrls: ['./lessons-list.component.css']
+})
+export class LessonsListComponent {
+
+    @Input()
+    lessons: Lesson[];
+
+    @Output()
+    selected = new EventEmitter<Lesson>();
+
+
+
+    select(lesson:Lesson) {
+        this.selected.next(lesson);
+    }
+
+}
+
+
+
---
+++ src/app/shared/model/lesson.ts
@@ -0,0 +1,12 @@
+export interface Lesson {
+    id: string;
+    description: string;
+    seqNo: number;
+    duration: string;
+    url?: string;
+    tags?: string;
+    pro?: boolean;
+    longDescription?: string;
+    courseId?: string;
+    videoUrl?: string;
+}
